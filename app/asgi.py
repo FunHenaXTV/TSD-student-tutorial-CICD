280ac06c79c138b0fd861dd52d6424e4031f2da3
Zubkov Dmitrii <dimazubkov2003@gmail.com>
2026-06-02 18:13:21 +0300

    break codestyle

--- app/asgi.py
+++ app/asgi.py
@@ -13,4 +13,10 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
-application = get_asgi_application()
+application = get_asgi_application(
+
+
+
+
+
+)
